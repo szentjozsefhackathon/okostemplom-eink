79375570b0b6280cc978695619547d682be87663
Pályi Lőrinc <palyilorinc33@gmail.com>
2024-05-25 21:01:12 +0200

    added logo for loading

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,14 +7,15 @@
 #define LILYGO_T5_V213
 
 #include <GxEPD.h>
+// GxEPD.h must be included first
 #include <GxDEPG0213BN/GxDEPG0213BN.h>  // 2.13" b/w  form DKE GROUP
 #include <boards.h>
 
 #include GxEPD_BitmapExamples
 #include <Adafruit_GFX.h>
-#include <U8g2_for_Adafruit_GFX.h>
 #include <GxIO/GxIO.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <U8g2_for_Adafruit_GFX.h>
 
 GxIO_Class io(SPI, EPD_CS, EPD_DC, EPD_RSET);
 GxEPD_Class display(io, EPD_RSET, EPD_BUSY);
@@ -43,25 +44,58 @@ void setup(void) {
     display.init();  // enable diagnostic output on Serial
     u8Display.begin(display);
 
-
     Serial.println("setup done");
+    displayHackathonLogo();
     display.setRotation(1);
     displayWidth = display.width();
     displayHeight = display.height();
     setupFont(u8Display, u8g2_font_helvB10_te);
     lineHeight = u8Display.getFontAscent() + abs(u8Display.getFontDescent());
-    Serial.print("Ascent: "); Serial.print(u8Display.getFontAscent());
-    Serial.print("Descent: "); Serial.print(u8Display.getFontDescent());
-    Serial.print("Line: "); Serial.println(lineHeight);
+    Serial.print("Ascent: ");
+    Serial.print(u8Display.getFontAscent());
+    Serial.print("Descent: ");
+    Serial.print(u8Display.getFontDescent());
+    Serial.print("Line: ");
+    Serial.println(lineHeight);
 
-    display.fillRoundRect(5, 5, displayWidth-10, displayHeight-10, 10, GxEPD_BLACK);
-    display.fillRoundRect(8, 8, displayWidth-16, displayHeight-16, 10, GxEPD_WHITE);
-    u8Display.setCursor(displayWidth/4, displayHeight/2);
+    display.fillRoundRect(5, 5, displayWidth - 10, displayHeight - 10, 10, GxEPD_BLACK);
+    display.fillRoundRect(8, 8, displayWidth - 16, displayHeight - 16, 10, GxEPD_WHITE);
+    u8Display.setCursor(displayWidth / 4, displayHeight / 2);
     u8Display.println("Helló, Szent József");
-    u8Display.setCursor(displayWidth/4, displayHeight/2 + lineHeight);
+    u8Display.setCursor(displayWidth / 4, displayHeight / 2 + lineHeight);
     u8Display.println(" Hackathon v2.0");
     display.update();
 }
 
 void loop() {
+}
+
+void displayHackathonLogo() {
+    display.eraseDisplay();
+    setupFont(u8Display, u8g2_font_t0_11b_mn);
+    display.setRotation(1);
+    auto displayWidth = display.width();
+    auto displayHeight = display.height();
+    auto lineWidth = u8Display.getUTF8Width("00011000");
+    auto lineHeight = u8Display.getFontAscent();
+    auto textHeight = 8 * lineHeight;
+    int16_t cursorY = (displayHeight - textHeight) / 2;
+    u8Display.setCursor((displayWidth - lineWidth) / 2, cursorY);
+    u8Display.println("00011000");
+    cursorY += lineHeight;
+    u8Display.setCursor((displayWidth - lineWidth) / 2, cursorY);
+    u8Display.println("00011000");
+    cursorY += lineHeight;
+    u8Display.setCursor((displayWidth - lineWidth) / 2, cursorY);
+    u8Display.println("11111111");
+    cursorY += lineHeight;
+    u8Display.setCursor((displayWidth - lineWidth) / 2, cursorY);
+    u8Display.println("00011000");
+    cursorY += lineHeight;
+    u8Display.setCursor((displayWidth - lineWidth) / 2, cursorY);
+    u8Display.println("00011000");
+    cursorY += lineHeight;
+    u8Display.setCursor((displayWidth - lineWidth) / 2, cursorY);
+    u8Display.println("00011000");
+    display.update();
 }
